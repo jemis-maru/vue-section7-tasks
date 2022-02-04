29ad6b2d11db6acccc8503252cb82086025cbbf1
jemis-maru <jemis.maru@bacancy.com>
2022-02-04 17:44:05 +0530

    Section-7 completed.

---
+++ src/App.vue
@@ -0,0 +1,90 @@
+<template>
+    <header>
+      <h1>FriendList</h1>
+    </header>
+    <div v-for="fr in friendsArr" :key="fr.id">
+        <FriendData :fr = 'fr' />
+    </div>
+</template>
+
+<script>
+import FriendData from './components/FriendData.vue';
+
+export default {
+    name: 'App',
+    components:{
+        FriendData,
+    },
+    data(){
+        return{
+            friendsArr: [
+                {
+                    id: 'abc',
+                    name: 'Abc Def',
+                    phNo: '1234567890',
+                    emailAdd: 'example@example.com',
+                },{
+                    id: 'pqr',
+                    name: 'Pqr Rst',
+                    phNo: '1234567890',
+                    emailAdd: 'example@example.com',
+                },
+            ],
+        }
+    }
+}
+</script>
+
+<style>
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+li {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+
+button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+button:hover,
+button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+</style>
---
+++ src/components/FriendData.vue
@@ -0,0 +1,43 @@
+<template>
+    <ul>
+        <li>
+            <h2>{{ fr.name }}</h2>
+            <button @click="toggleDetails">{{ btnTextFun }}</button>
+            <ul v-show="detailsToggle">
+                <li><strong>Phone: </strong>{{ fr.phNo }}</li>
+                <li><strong>Email: </strong>{{ fr.emailAdd }}</li>
+            </ul>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'FriendData',
+    props:{
+        fr: {
+            type: Object,
+        },
+    },
+    data(){
+        return{
+            detailsToggle: false,
+        }
+    },
+    methods:{
+        toggleDetails(){
+            this.detailsToggle = !this.detailsToggle;
+        },
+    },
+    computed:{
+        btnTextFun(){
+            if(this.detailsToggle){
+                return 'Hide details';
+            }
+            else{
+                return 'Show details';
+            }
+        },
+    },
+}
+</script>
